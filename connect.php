22d6f83c85ee70520b46916f39ca0ea16a4babf4
Mohamed Samatar <mohamedsamatar100@gmail.com>
2019-12-12 11:38:40 -0800

    Add files via upload

--- connect.php
+++ connect.php
@@ -1,5 +1,9 @@
 <?php
-  $dbconnection = mysqli_connect("localhost","root","","bulletin");
+  $host = 'aa10qog1gcbw0x2.cpmkzsexhlp4.us-west-1.rds.amazonaws.com'
+  $user = 'root'
+  $pass = 'password'
+  $db_name = 'bulletin'
+  $dbconnection = mysqli_connect($host, $user, $pass, $db_name);
 
   if (mysqli_connect_errno()){
     die("Database connection failed: " . mysqli_connect_error());
